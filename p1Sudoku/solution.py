017eeb311bd3fe1c2fda42d4c4ec77c1765da597
VectorLu <vectorpinglu@outlook.com>
2017-02-21 11:54:20 +0800

    Use DFS and constraint propagation to solve every Sudoku

--- p1Sudoku/solution.py
+++ p1Sudoku/solution.py
@@ -1,4 +1,14 @@
 from utils import *
+initial_digits = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+
+initial_harder_digits = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+
+"""
+Hard Sudoku:
+
+4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......
+
+"""
 
 def grid_values(grid):
     """
@@ -22,7 +32,7 @@ def grid_values(grid):
     return dict(zip(boxes, values))
 
 
-initial_digits = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+
 
 initial_sudoku = grid_values(initial_digits)
 # display(temp_sudoku)
@@ -107,3 +117,26 @@ def reduce_puzzle(values):
 # display(initial_sudoku)
 # stalled_values = reduce_puzzle(initial_sudoku)
 # display(stalled_values)
+
+def search(values):
+    "Using depth-first search and paopagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+
+    # Choose one of unfilled squares with the fewest possibilities
+    n, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+
+    # Now use recursion to solve each one of the Resulting sudokus, and if one returns a value (not False), return that answer!
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+
+display(search(grid_values(initial_harder_digits)))
